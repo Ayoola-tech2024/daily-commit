b3d90a4da055d3a71ef6f427a5cca558d152e49f
Ayoola Damisile <damisileayoola@gmail.com>
2026-07-14 02:57:42 +0100

    fix: remove arrow char from link, test markdown rendering

--- scripts/build.sh
+++ scripts/build.sh
@@ -201,7 +201,7 @@ $FEATURE_DESC
 
 $GOAL_BAR $GOAL_PCT%
 
-[View all builds →](../index.md)
+[View all builds](../index.md)
 
 ---
 
@@ -302,7 +302,7 @@ $PROGRESS_BAR $PERCENTAGE%
 
 [View today's entry →](./content/$DATE_STR.md)
 
-[View all builds →](../index.md)
+[View all builds](../index.md)
 
 ---
 
